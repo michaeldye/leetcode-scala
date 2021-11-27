b9e31150ed6a6ff8b87a0695d4a66ee395c14259
michael dye <m-github@divisive.info>
2021-11-27 13:27:15 -0700

    updated ITS, working

--- src/test/scala/leetcode/ITSSpec.scala
+++ src/test/scala/leetcode/ITSSpec.scala
@@ -1,7 +1,7 @@
 package leetcode
 
 import org.scalatest.flatspec.AnyFlatSpec
-
+import org.scalatest.matchers.should.Matchers._
 
 class ITSSpec extends AnyFlatSpec {
 
@@ -19,10 +19,22 @@ class ITSSpec extends AnyFlatSpec {
 
   "ITS" should "not find triplet among [6,7,1,2]" in {
     assert(!ITS.increasingTriplet(Array(6,7,1,2)))
-
   }
 
   "ITS" should "find triple among [20,100,10,12,5,13]" in {
     assert(ITS.increasingTriplet(Array(20,100,10,12,5,13)))
   }
+
+  "ITS" should "find triple among [1,5,0,4,1,3]" in {
+    assert(ITS.increasingTriplet(Array(1,5,0,4,1,3)))
+  }
+
+//  "ITS permute" should "expand subs from [6,7,1,2]" in {
+//    val tt = ITS.permute(Array(6,7,1,2))
+//
+//    tt should contain(Array(6,7,1,2))
+//    tt should contain(Array(7,1,2))
+//    tt should contain(Array(1,2))
+//    tt should contain(Array(2))
+//  }
 }
